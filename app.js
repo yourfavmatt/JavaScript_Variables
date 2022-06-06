c97469699560edb8ba415caccffe3a3f2a8a8f01
matt Klein <mwklein212@gmail.com>
2022-06-06 16:18:29 -0400

    completed exercise

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+const firstName = "Matt";
+let lastName = "Klein";
+var age = 38;
 
-// Exercise 1
 
-// Exercise 2
+lastName = "Gamgee";
+age = "24";
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let iStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${language} was created in ${createdYear}.`);
+console.log(`${lastName} was born ${createdYear}`)
